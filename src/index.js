b9c1e2ce8367d2a3a655efa69b6cf2a3849ac4e7
jbagration <blissdery@gmail.com>
2023-02-24 15:15:21 +0500

    modified:   package-lock.json 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if (!matrix) return []; // если матрица пустая, возвращаем пустой массив
+  
+  const result = []; // создаем переменную с пустым массивом
+  for (let i = 0; i < matrix.length; i++) { // цикл для обхода по массиву
+    const row = matrix[i]; // создаем переменную с частью массива
+    if (i % 2 === 0) { 
+      // четные
+      for (let j = 0; j < row.length; j++) {
+        result.push(row[j]); // добавляем числа в массив
+      }
+    } else {
+      // нечетные
+      for (let j = row.length - 1; j >= 0; j--) {
+        result.push(row[j]);
+      }
+    }
+  }
+  
+  return result;
 }
